e6ecea988e0744c9f1785378ee4003a179cc893f
Abdulwahab Nasir <weybansky@gmail.com>
2019-08-28 23:34:07 +0100

    added  new files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function open_menu () {
+	document.getElementById('mobile_menu').style.display = 'block';
+}
+
+function close_mobile_menu () {
+	document.getElementById('mobile_menu').style.display = 'none';
+}
